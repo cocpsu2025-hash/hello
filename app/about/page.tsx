7aeb3f8709614006fe88d9ebe4b120f2267509d0
Warodom Werapun <wwarodom@gmail.com>
2026-02-25 11:04:12 +0700

    init repo

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+// const AboutPage = () => <>About</>
+
+function AboutPage() {
+    return (
+        <div className="flex justify-center items-center min-h-screen">
+            <h1 className="text-[8rem] font-bold text-shadow-lg">
+                About
+            </h1>
+        </div>
+    )
+}
+
+
+export default AboutPage
