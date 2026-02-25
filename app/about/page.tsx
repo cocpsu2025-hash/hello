99becae7fa716797d57820e101dd4523fc10e363
Warodom Werapun <wwarodom@gmail.com>
2026-02-25 11:29:11 +0700

    add an image from local drive

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,19 @@
+import Image from 'next/image'
+
 // const AboutPage = () => <>About</>
 
 function AboutPage() {
     return (
-        <div className="flex justify-center items-center min-h-screen">
-            <h1 className="text-[8rem] font-bold text-shadow-lg">
-                About
-            </h1>
+        <div>
+            <div className="flex justify-center items-center min-h-screen">
+                <h1 className="text-[8rem] font-bold text-shadow-lg">
+                    About
+                    <div>
+                        <Image src="/cat.jpeg" height={300} width={350} alt="cat" />
+                    </div>
+                </h1>
+            </div>
+
         </div>
     )
 }
