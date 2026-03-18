4d04e4952f71ec922ac12cbf328c09da0d38b51a
Warodom Werapun <wwarodom@gmail.com>
2026-03-18 11:18:27 +0700

    edit todo

--- app/todo/[id]/page.tsx
+++ app/todo/[id]/page.tsx
@@ -1,3 +1,29 @@
-export default function EditTodo() {
-    return <> ID </>
+// import { useActionState } from "react"
+import { URL } from "../constant"
+
+export default async function EditTodo({ params }: { params: Promise<{ id: string }> }) {
+
+    // const id = (await params).id
+    const { id } = (await params)  // destructuring
+
+    const todo = await (await fetch(`${URL}/${id}`)).json()
+
+    console.log("Todo: ", todo)
+
+    // const [state, action] = useActionState(updateTodo, {})
+
+    return <> ID: {id}
+        <h1>Edit Todo</h1>
+        <div>
+            <form action="/">
+                <input
+                    className="border p-2 m-2"
+                    type="text" name="task" defaultValue={todo.task} />
+                <input className="border p-2 m-2"
+                    type="number" name="time" defaultValue={todo.time} />
+                <button className="border p-2 m-2"
+                    type="submit">Update</button>
+            </form>
+        </div>
+    </>
 }
